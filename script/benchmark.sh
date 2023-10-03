56e29a865b12917655583a9fc002b7bf002550a4
Ryan Boyer <ryanboyerpublic@gmail.com>
2023-10-02 21:49:31 -0500

    Benchmark setup

--- script/benchmark.sh
+++ script/benchmark.sh
@@ -4,4 +4,15 @@ mkdir benchmark_output
 benchmark_run=$(date +%Y%m%d_%H%M%S)
 benchmark_folder=benchmark_output/"$benchmark_run"
 mkdir "$benchmark_folder"
-hyperfine --warmup 100  -N --export-json "$benchmark_folder"/moby_dick.json 'wc example_txt_files/moby_dick.txt'
+
+echo "Moby Dick"
+hyperfine --warmup 100  -N --export-json "$benchmark_folder"/moby_dick.json 'wc benchmark_texts/moby_dick.txt' './target/release/ryan_wc benchmark_texts/moby_dick.txt'
+
+echo "KJV 1x"
+hyperfine --warmup 10  -N --export-json "$benchmark_folder"/kjv_1x.json 'wc benchmark_texts/kjv_1x.txt' './target/release/ryan_wc benchmark_texts/kjv_1x.txt'
+
+echo "KJV 10x"
+hyperfine --warmup 10  -N --export-json "$benchmark_folder"/kjv_10x.json 'wc benchmark_texts/kjv_10x.txt' './target/release/ryan_wc benchmark_texts/kjv_10x.txt'
+
+echo "KJV 1x"
+hyperfine --warmup 10  -N --export-json "$benchmark_folder"/kjv_100x.json 'wc benchmark_texts/kjv_100x.txt' './target/release/ryan_wc benchmark_texts/kjv_100x.txt'
---
+++ script/download_text_files.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+mkdir benchmark_texts
+MYTMPDIR="$(mktemp -d)"
+trap 'rm -rf -- "$MYTMPDIR"' EXIT
+
+kjvdir=$MYTMPDIR/kjv
+mkdir "$kjvdir"
+echo "Running: \`curl -Lo "$kjvdir"/tmp_kjv.zip \"https://archive.org/compress/kjv-text-files/formats=TEXT&file=/kjv-text-files.zip\"\`"
+curl -Lo $kjvdir/tmp_kjv.zip "https://archive.org/compress/kjv-text-files/formats=TEXT&file=/kjv-text-files.zip"
+echo "Running unzip $kjvdir/tmp_kjv.zip -d $kjvdir"
+unzip -q "$kjvdir"/tmp_kjv.zip -d "$kjvdir"
+
+echo "Generating KJV 1x"
+rm -f benchmark_texts/kjv_1x.txt
+echo "$kjvdir"
+cat "$kjvdir"/*.txt > benchmark_texts/kjv_1x.txt
+
+echo "Generating KJV 10x"
+rm -f benchmark_texts/kjv_10x.txt
+for i in {1..5}
+do
+   cat benchmark_texts/kjv_1x.txt >> benchmark_texts/kjv_10x.txt
+done
+
+echo "Generating KJV 100x"
+rm -f benchmark_texts/kjv_100x.txt
+for i in {1..5}
+do
+   cat benchmark_texts/kjv_10x.txt >> benchmark_texts/kjv_100x.txt
+done
+
